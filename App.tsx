2f201c552159f1f463f52e091e2078d61508dcb3
msvalandro <msvalandro@proton.me>
2024-08-09 16:56:23 +0100

    chore: config mongodb atlas

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
+import { REALM_APP_ID } from '@env'
 import {
   Roboto_400Regular,
   Roboto_700Bold,
   useFonts,
 } from '@expo-google-fonts/roboto'
+import { AppProvider, UserProvider } from '@realm/react'
 import { StatusBar } from 'react-native'
 import { ThemeProvider } from 'styled-components/native'
 
 import { Loading } from './src/components/Loading'
+import { Home } from './src/screens/Home'
 import { SignIn } from './src/screens/SignIn'
 import theme from './src/theme'
 
@@ -18,13 +21,18 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <SignIn />
-    </ThemeProvider>
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+
+        <UserProvider fallback={SignIn}>
+          <Home />
+        </UserProvider>
+      </ThemeProvider>
+    </AppProvider>
   )
 }
