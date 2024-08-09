8d0fcb10ebdb6e4c5476ea9d9af165d6840c4a59
msvalandro <msvalandro@proton.me>
2024-08-09 09:33:43 +0100

    chore: config fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto'
+import { StatusBar } from 'react-native'
 import { ThemeProvider } from 'styled-components/native'
 
+import { Loading } from './src/components/Loading'
 import { SignIn } from './src/screens/SignIn'
 import theme from './src/theme'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
+
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <SignIn />
     </ThemeProvider>
   )
