1737799c5b931411bc024bff2f66683df7dcd862
msvalandro <msvalandro@proton.me>
2024-08-11 22:58:37 +0100

    feat: fetch object data on arrival screen

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,9 +1,12 @@
 import { useRoute } from '@react-navigation/native'
 import { X } from 'phosphor-react-native'
+import { BSON } from 'realm'
 
 import { Button } from '../../components/Button'
 import { ButtonIcon } from '../../components/ButtonIcon'
 import { Header } from '../../components/Header'
+import { useObject } from '../../libs/realm'
+import { History } from '../../libs/realm/schemas/History'
 import {
   ArrivalContainer,
   Content,
@@ -21,19 +24,18 @@ export function Arrival() {
   const route = useRoute()
   const { id } = route.params as RouteParamsProps
 
+  const history = useObject(History, new BSON.UUID(id) as unknown as string)
+
   return (
     <ArrivalContainer>
       <Header title="Chegada" />
 
       <Content>
         <Label>Placa do veículo</Label>
-        <LicensePlate>XXX0000</LicensePlate>
+        <LicensePlate>{history?.license_plate}</LicensePlate>
 
         <Label>Finalidade</Label>
-        <Description>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt,
-          sed!
-        </Description>
+        <Description>{history?.description}</Description>
 
         <Footer>
           <ButtonIcon icon={X} />
