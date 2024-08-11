77d633428d54ef5abf29b357acc192d7e1eb6581
msvalandro <msvalandro@proton.me>
2024-08-11 22:38:55 +0100

    feat: fetch vehicle in use from realm db

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,22 +1,52 @@
 import { useNavigation } from '@react-navigation/native'
+import { useEffect, useState } from 'react'
+import { Alert } from 'react-native'
 
 import { CarStatus } from '../../components/CarStatus'
 import { HomeHeader } from '../../components/HomeHeader'
+import { useQuery } from '../../libs/realm'
+import { History } from '../../libs/realm/schemas/History'
 import { Content, HomeContainer } from './styles'
 
 export function Home() {
+  const [vehicleInUse, setVehicleInUse] = useState<History | null>(null)
+
   const navigation = useNavigation()
+
+  const history = useQuery(History)
 
   function handleRegisterMovement() {
     navigation.navigate('departure')
   }
+
+  function fetchVehicle() {
+    try {
+      const vehicle = history.filtered("status = 'departure'")[0]
+
+      setVehicleInUse(vehicle)
+    } catch (error) {
+      console.log(error)
+
+      Alert.alert(
+        'Veículo em uso',
+        'Não foi possível carregar o veículo em uso.',
+      )
+    }
+  }
+
+  useEffect(() => {
+    fetchVehicle()
+  }, [])
 
   return (
     <HomeContainer>
       <HomeHeader />
 
       <Content>
-        <CarStatus onPress={handleRegisterMovement} />
+        <CarStatus
+          licensePlate={vehicleInUse?.license_plate}
+          onPress={handleRegisterMovement}
+        />
       </Content>
     </HomeContainer>
   )
