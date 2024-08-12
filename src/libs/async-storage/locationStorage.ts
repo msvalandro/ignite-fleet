a9f3bc100bcaa29961f6842cade411444be32d7b
msvalandro <msvalandro@proton.me>
2024-08-12 21:26:42 +0100

    refactor: create methods to get/save location on storage

---
+++ src/libs/async-storage/locationStorage.ts
@@ -0,0 +1,25 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+const STORAGE_KEY = '@ignitefleet/location'
+
+interface LocationProps {
+  latitude: number
+  longitude: number
+  timestamp: number
+}
+
+export async function getStorageLocation() {
+  const storage = await AsyncStorage.getItem(STORAGE_KEY)
+
+  const response: LocationProps[] = storage ? JSON.parse(storage) : []
+
+  return response
+}
+
+export async function saveStorageLocation(newLocation: LocationProps) {
+  const storage = await getStorageLocation()
+
+  storage.push(newLocation)
+
+  await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(storage))
+}
